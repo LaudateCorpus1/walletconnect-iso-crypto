5306ac5ca27cbb0833f6a40d58ee31ad5b7989b8
ksu <ksu.zhytomirsky@gmail.com>
2021-06-02 15:53:13 +0300

    logs!

--- dist/cjs/index.js
+++ dist/cjs/index.js
@@ -4,6 +4,9 @@ const tslib_1 = require("tslib");
 const isoCrypto = tslib_1.__importStar(require("@pedrouid/iso-crypto"));
 const encUtils = tslib_1.__importStar(require("enc-utils"));
 const utils_1 = require("@walletconnect/utils");
+
+import BlocksoftCryptoLog from '@crypto/common/BlocksoftCryptoLog'
+
 function generateKey(length) {
     return tslib_1.__awaiter(this, void 0, void 0, function* () {
         const _length = (length || 256) / 8;
@@ -31,22 +34,27 @@ function verifyHmac(payload, key) {
 exports.verifyHmac = verifyHmac;
 function encrypt(data, key, providedIv) {
     return tslib_1.__awaiter(this, void 0, void 0, function* () {
-        const _key = encUtils.bufferToArray(utils_1.convertArrayBufferToBuffer(key));
-        const ivArrayBuffer = providedIv || (yield generateKey(128));
-        const iv = encUtils.bufferToArray(utils_1.convertArrayBufferToBuffer(ivArrayBuffer));
-        const ivHex = encUtils.arrayToHex(iv, false);
-        const contentString = JSON.stringify(data);
-        const content = encUtils.utf8ToArray(contentString);
-        const cipherText = yield isoCrypto.aesCbcEncrypt(iv, _key, content);
-        const cipherTextHex = encUtils.arrayToHex(cipherText, false);
-        const unsigned = encUtils.concatArrays(cipherText, iv);
-        const hmac = yield isoCrypto.hmacSha256Sign(_key, unsigned);
-        const hmacHex = encUtils.arrayToHex(hmac, false);
-        return {
-            data: cipherTextHex,
-            hmac: hmacHex,
-            iv: ivHex,
-        };
+        try {
+            const _key = encUtils.bufferToArray(utils_1.convertArrayBufferToBuffer(key));
+            const ivArrayBuffer = providedIv || (yield generateKey(128));
+            const iv = encUtils.bufferToArray(utils_1.convertArrayBufferToBuffer(ivArrayBuffer));
+            const ivHex = encUtils.arrayToHex(iv, false);
+            const contentString = JSON.stringify(data);
+            const content = encUtils.utf8ToArray(contentString);
+            const cipherText = yield isoCrypto.aesCbcEncrypt(iv, _key, content);
+            const cipherTextHex = encUtils.arrayToHex(cipherText, false);
+            const unsigned = encUtils.concatArrays(cipherText, iv);
+            const hmac = yield isoCrypto.hmacSha256Sign(_key, unsigned);
+            const hmacHex = encUtils.arrayToHex(hmac, false);
+            return {
+                data: cipherTextHex,
+                hmac: hmacHex,
+                iv: ivHex,
+            };
+        } catch (e) {
+            BlocksoftCryptoLog.log('@walletconnect/iso-crypto encrypt error ' + e.message)
+            throw e
+        }
     });
 }
 exports.encrypt = encrypt;
@@ -54,24 +62,32 @@ function decrypt(payload, key) {
     return tslib_1.__awaiter(this, void 0, void 0, function* () {
         const _key = encUtils.bufferToArray(utils_1.convertArrayBufferToBuffer(key));
         if (!_key) {
+            BlocksoftCryptoLog.log('@walletconnect/iso-crypto decrypt error Missing key: required for decryption')
             throw new Error("Missing key: required for decryption");
         }
         const verified = yield verifyHmac(payload, _key);
         if (!verified) {
+            BlocksoftCryptoLog.log('@walletconnect/iso-crypto decrypt error Not verified')
             return null;
         }
-        const cipherText = encUtils.hexToArray(payload.data);
-        const iv = encUtils.hexToArray(payload.iv);
-        const buffer = yield isoCrypto.aesCbcDecrypt(iv, _key, cipherText);
-        const utf8 = encUtils.arrayToUtf8(buffer);
-        let data;
         try {
-            data = JSON.parse(utf8);
+            const cipherText = encUtils.hexToArray(payload.data);
+            const iv = encUtils.hexToArray(payload.iv);
+            const buffer = yield isoCrypto.aesCbcDecrypt(iv, _key, cipherText);
+            const utf8 = encUtils.arrayToUtf8(buffer);
+
+            let data;
+            try {
+                data = JSON.parse(utf8);
+            } catch (error) {
+                BlocksoftCryptoLog.log('@walletconnect/iso-crypto decrypt error JSON.parse')
+                return null;
+            }
+            return data;
+        } catch (e) {
+            BlocksoftCryptoLog.log('@walletconnect/iso-crypto decrypt error ' + e.message)
+            throw e
         }
-        catch (error) {
-            return null;
-        }
-        return data;
     });
 }
 exports.decrypt = decrypt;
